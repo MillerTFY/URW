55be94a0feed00833c05984aa874e3fd321e9ae5
TFY2022 <99530167+TFY2022@users.noreply.github.com>
2022-03-05 15:53:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,89 +1,44 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "1c2be92a",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from flask import Flask\n",
-    "app = Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "043d503d",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "   WARNING: This is a development server. Do not use it in a production deployment.\n",
-      "   Use a production WSGI server instead.\n",
-      " * Debug mode: off\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n"
-     ]
-    }
-   ],
-   "source": [
-    "from flask import request, render_template\n",
-    "from textblob import TextBlob\n",
-    "\n",
-    "   \n",
-    "@app.route(\"/\", methods=[\"GET\", \"POST\"])\n",
-    "def index():\n",
-    "    if request.method == \"POST\":\n",
-    "        text = request.form.get(\"text\")\n",
-    "        print(text)\n",
-    "        r = TextBlob(text).sentiment\n",
-    "        return(render_template(\"index.html\", results=r))\n",
-    "    else:\n",
-    "        return(render_template(\"index.html\", results=\"2\"))\n",
-    "\n",
-    "\n",
-    "if __name__==\"__main__\": \n",
-    "    app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "0f307c86",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install textblob')
+
+
+# In[2]:
+
+
+from textblob import TextBlob
+
+
+# In[3]:
+
+
+f = open("news.txt")
+
+
+# In[4]:
+
+
+d = f.read()
+
+
+# In[5]:
+
+
+print(d)
+
+
+# In[6]:
+
+
+TextBlob(d).sentiment
+
+
+# In[ ]:
+
+
+
+
