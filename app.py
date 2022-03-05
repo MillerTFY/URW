061d6d211a7ec6f692523145fd5eb5e3db7202f8
TFY2022 <99530167+TFY2022@users.noreply.github.com>
2022-03-05 13:42:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,224 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "dc87076f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask\n",
+    "app = Flask(__name__)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6ebe5810",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " * Serving Flask app \"__main__\" (lazy loading)\n",
+      " * Environment: production\n",
+      "   WARNING: This is a development server. Do not use it in a production deployment.\n",
+      "   Use a production WSGI server instead.\n",
+      " * Debug mode: off\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
+      "127.0.0.1 - - [05/Mar/2022 12:03:58] \"\u001b[37mGET / HTTP/1.1\u001b[0m\" 200 -\n",
+      "127.0.0.1 - - [05/Mar/2022 12:04:02] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "very bad\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:04:48] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "very bad\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:04:52] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "super\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:00] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "fuck\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:10] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "theng\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:19] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Russia\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:21] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:24] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Russia\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:36] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "bomb\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "127.0.0.1 - - [05/Mar/2022 12:05:41] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "very bad\n"
+     ]
+    }
+   ],
+   "source": [
+    "from flask import request, render_template\n",
+    "from textblob import TextBlob\n",
+    "\n",
+    "   \n",
+    "@app.route(\"/\", methods=[\"GET\", \"POST\"])\n",
+    "def index():\n",
+    "    if request.method == \"POST\":\n",
+    "        text = request.form.get(\"text\")\n",
+    "        print(text)\n",
+    "        r = TextBlob(text).sentiment\n",
+    "        return(render_template(\"index.html\", results=r))\n",
+    "    else:\n",
+    "        return(render_template(\"index.html\", results=\"2\"))\n",
+    "\n",
+    "\n",
+    "if __name__==\"__main__\": \n",
+    "    app.run()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "576953c1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
